739c1416657be90b96d74239682ae5ac791ced83
PabloOro27 <pabloorozco2712@gmail.com>
2023-06-13 17:55:19 -0600

    V1 consumo de apis

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import Card from '../../components/Card';
 
 const Home = () => {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+    .then(response => response.json())
+    .then (data => setItems(data))
+  }, []);
+
     return (
-      <>
-        <h1>Home</h1>
-        <Card />
-      </>
+      <div className='home flex w-full h-full mx-5'>
+        {
+          items && items.map(() => {
+           return <Card />
+          })
+        }
+      </div>
     );
 };
 
